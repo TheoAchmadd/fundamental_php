d69da80a3752dfafec8c5bda306f466aff160650
TheoAchmadd <theo.achmad78@smk.belajar.id>
2025-02-12 17:06:49 +0700

    add : looping

---
+++ looping/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div>
+        <label for="">Baris :</label>
+        <input type="text" name="" id="">
+    </div>
+
+    <div>
+        <label for="">kolom :</label>
+        <input type="text" name="" id="">
+    </div>
+
+    <div>
+        <input type="submit" value="INPUT DATA">
+    </div>
+
+</body>
+</html>
