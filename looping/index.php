0de755ac3b705fb331db7ba8a424f0a19f1a54a7
TheoAchmadd <theo.achmad78@smk.belajar.id>
2025-02-12 23:16:33 +0700

    fix : text

--- looping/index.php
+++ looping/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Membuat Tabel Otomatis</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
     <style>
         body {
             margin: 2em;
@@ -30,22 +30,36 @@
     <?php
 
         if(isset($_POST['submit'])) {
-            $baris = $_POST['baris']
-            $kolom = $_POST['kolom']
+            $baris = $_POST['baris'];
+            $kolom = $_POST['kolom'];
     ?>
 
     <table border=1 style="width:75%;">
 
         <?php
-            for($i = 1;)
+            for($i = 1; $i <= $baris; $i++) {
         ?>
-
-        <?php
+        <tr>
+            <?php
+            for($y = 1; $y <= $kolom; $y++) {
+            ?>
+            <td>
+                &nbsp;
+            </td>
+            <?php
             }
+            ?>
+        </tr>
+        <?php
+        }
         ?>
 
     </table>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <?php
+            }
+        ?>
+
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> -->
 </body>
 </html>
